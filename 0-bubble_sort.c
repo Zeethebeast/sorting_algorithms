7da85403510695ee104d1d5c9671d1d8f7f05ce6
zeethebeast <chimasamuel6@gmail.com>
2024-05-05 06:26:36 -0700

    sorting algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,24 @@
 #include "sort.h"
-void bubble_sort(int *array, size_t size) 
+/*
+ * main - entry point
+ *@size_t: integer variables
+ * Return: nothng
+ */
+void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 }
